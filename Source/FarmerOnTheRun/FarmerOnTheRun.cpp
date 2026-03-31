d4c0e1b4c981a58ea7409e71f03d86e3ee41dd22
0shiros <oli2014@gmx.fr>
2026-03-31 15:26:59 +0200

    Initial commit

---
+++ Source/FarmerOnTheRun/FarmerOnTheRun.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FarmerOnTheRun.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FarmerOnTheRun, "FarmerOnTheRun" );
